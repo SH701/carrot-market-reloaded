4c95d2054f7a0fbf354e8bee5dafe9a3c44acea0
SH701 <125341405+SH701@users.noreply.github.com>
2025-05-12 02:49:34 +0900

    fixededed

--- app/api/auth/github/complete/route.ts
+++ app/api/auth/github/complete/route.ts
@@ -1,75 +1,81 @@
-import db from "@/lib/db";
-import { accessTokenGithub , getUserEmailGithub, getUserProfileGithub } from "@/lib/githubAuth";
-import { LoginSession } from "@/lib/session";
-import { makeRandomUsername } from "@/lib/username";
-import { redirect } from "next/navigation";
-import { NextRequest,NextResponse } from "next/server";
+import db from '@/lib/db';
+import {
+  accessTokenGithub,
+  getUserEmailGithub,
+  getUserProfileGithub,
+} from '@/lib/githubAuth';
+import { LoginSession } from '@/lib/session';
+import { NextRequest, NextResponse } from 'next/server';
 
-async function generateAvailableUsername(baseUsername: string): Promise<string> {
-    const maxTries = 10;
-    let tries = 0;
-  
-    while (tries < maxTries) {
-      const newUsername = makeRandomUsername(baseUsername); 
-      const existingUser = await db.user.findUnique({
-        where: {
-          username: newUsername,
-        },
-        select: {
-          id: true,
-        },
-      });
-     if(!existingUser){
-        return newUsername
-     }else{
-        tries++;
-     }
-    }
-    throw new Error("Cannot generate available username, please try manually.");
-  }
-  
-
-export async function GET(request:NextRequest){
-    const code = request.nextUrl.searchParams.get("code")
-    if(!code){
-        return NextResponse.json({error:"Not code"},{status:400})
-    }
-    const {error,access_token} = await accessTokenGithub (code);
-    if(error){
-        return new NextResponse(null,{
-            status:400
-        })
-    }
-    const {id,avatar_url,login} = await getUserProfileGithub(access_token);
-    const {email} = await getUserEmailGithub(access_token);
-
-    const user = await db.user.findUnique({
-        where:{github_id:id+"",},
-        select:{id:true,}
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get('code');
+  if (!code) {
+    return new Response(null, {
+      status: 400,
     });
-    if(user){
-        return LoginSession(user)
-    }
-    const ename = await db.user.findUnique({
-        where:{email},
-        select:{id:true},
-    })
-    if(ename){
-        return NextResponse.json({
-            error:"Email is already registerd."
-        },{status:400})
-    }
-    const finalUsername = await generateAvailableUsername(login);
-    const newUser= await db.user.create({
-        data:{
-            username:finalUsername,
-            github_id:id+"",
-            avatar:avatar_url,
-            email:email,
-        },
-        select:{id:true},
-    })
-    await LoginSession(newUser)
-    return NextResponse.redirect(new URL("/profile", request.url));
+  }
 
+  const access_token = await accessTokenGithub(code);
+
+  const { id, avatar_url, login } = await getUserProfileGithub(access_token);
+  const emailData = await getUserEmailGithub(access_token);
+  const email = emailData.email;
+
+  console.log(email);
+
+  // GitHub ID로 사용자 조회
+  const user = await db.user.findUnique({
+    where: {
+      github_id: id + '',
+    },
+    select: {
+      id: true,
+      username: true,
+    },
+  });
+
+  if (user) {
+    return LoginSession(user);
+  }
+
+  const findUsername = await db.user.findUnique({
+    where: {
+      username: login,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const findEmail = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (findEmail) {
+    return new NextResponse('Email is already in use.', {
+      status: 400,
+    });
+  }
+
+  const timeStamp = Date.now();
+
+  const newUser = await db.user.create({
+    data: {
+      username: findUsername ? `${login}${timeStamp}` : login,
+      github_id: id + '',
+      avatar: avatar_url,
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  // 새로 생성된 사용자로 Auth 처리
+  return LoginSession(newUser);
 }
